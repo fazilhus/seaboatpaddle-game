58b907c52985bc53137a8ea697f59bd1582105b0
fazilhus <khusnutdinov.fazil@gmail.com>
2024-02-02 10:46:03 +0100

    first iteration of physics-like controls

--- scripts/Boat.cs
+++ scripts/Boat.cs
@@ -1,73 +1,88 @@
 using Godot;
 using System;
 
-public partial class Boat : CharacterBody3D
+enum PaddleSide {
+    Left = 0,
+    Right = 1,
+}
+
+public partial class Boat : RigidBody3D
 {
     private Node3D _left_paddle;
     private Node3D _right_paddle;
+    private Node3D _left_force_point;
+    private Node3D _right_force_point;
     private Node3D _pivot;
-    private Timer _left_paddle_timer;
-    private Timer _right_paddle_timer;
 
-    private Vector3 lpf = new Vector3(0.5f, 0.0f, 1.0f).Normalized();
-    private float lpa = -15;
-    private Vector3 rpf = new Vector3(-0.5f, 0.0f, 1.0f).Normalized();
-    private float rpa = 15;
+    private PaddleSide _paddle_side;
 
-    private float power = 2;
+    private Vector3 _left_paddle_rotation_old;
+    private Vector3 _left_paddle_angular_velocity;
+    private Vector3 _right_paddle_rotation_old;
+    private Vector3 _right_paddle_angular_velocity;
 
     public override void _Ready()
     {
         _left_paddle = GetNode<Node3D>("LeftPaddlePivot");
         _right_paddle = GetNode<Node3D>("RightPaddlePivot");
-        _left_paddle_timer = GetNode<Timer>("LeftPaddleTimer");
-        _right_paddle_timer = GetNode<Timer>("RightPaddleTimer");
+        _left_force_point = GetNode<Node3D>("LeftPaddlePivot/ForcePoint");
+        _right_force_point = GetNode<Node3D>("RightPaddlePivot/ForcePoint");
         _pivot = GetNode<Node3D>("Pivot");
+        _paddle_side = PaddleSide.Left;
     }
 
     public override void _Process(double delta)
     {
-        if (Input.IsActionJustPressed("left_paddle") && _left_paddle_timer.IsStopped()) {
-            _left_paddle_timer.Start();
-        }
-        if (Input.IsActionJustPressed("right_paddle") && _right_paddle_timer.IsStopped()) {
-            _right_paddle_timer.Start();
+        if (Input.IsActionJustPressed("switch_side")) {
+            if (_paddle_side == PaddleSide.Left) {
+                _paddle_side = PaddleSide.Right;
+            }
+            else {
+                _paddle_side = PaddleSide.Left;
+            }
         }
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        Vector3 velocity = Velocity;
-        Vector3 rotation = _pivot.Rotation;
         Vector3 forward = _pivot.Basis.Z;
-        
-        Vector3 axis = new Vector3(0, 1, 0);
-        if (!_left_paddle_timer.IsStopped()) {
-            float delta_angle = Mathf.DegToRad(lpa) * (float)delta;
-            rotation = rotation + new Vector3(0, delta_angle, 0);
-            velocity += forward * lpf * power * (float)delta;
-            //velocity = velocity.Rotated(axis, delta_angle);
+        Vector3 left_paddle_forward = _left_paddle.Basis.Z;
+        Vector3 right_paddle_forward = _right_paddle.Basis.Z;
+
+        Vector3 input = Vector3.Zero;
+        input.Z = -Input.GetActionStrength("right") + Input.GetActionStrength("left");
+        input.X = -Input.GetActionStrength("backward") + Input.GetActionStrength("forward");
+
+
+        switch (_paddle_side) {
+            case PaddleSide.Left: {
+                _left_paddle_rotation_old = _left_paddle.Rotation;
+                _left_paddle.Rotation = new Vector3(input.X * 0.8f, 0, input.Z * 0.5f);
+                _left_paddle_angular_velocity = (_left_paddle.Rotation - _left_paddle_rotation_old) / (float)delta;
+
+                Vector3 force = new Vector3(_left_paddle_angular_velocity.Z, 0, -_left_paddle_angular_velocity.X);
+                if (_left_force_point.GlobalPosition.Y > 0) {
+                    force = Vector3.Zero;
+                }
+
+                ApplyForce(-0.125f * force * forward, _left_paddle.Position);
+                ApplyCentralForce(-4 * _left_paddle_angular_velocity.Sign().X * forward);
+                break;
+            }
+            case PaddleSide.Right: {
+                _right_paddle_rotation_old = _right_paddle.Rotation;
+                _right_paddle.Rotation = new Vector3(input.X * 0.8f, 0, input.Z * 0.5f);
+                _right_paddle_angular_velocity = (_right_paddle.Rotation - _right_paddle_rotation_old) / (float)delta;
+                Vector3 force = new Vector3(-_right_paddle_angular_velocity.Z, 0, -_right_paddle_angular_velocity.X);
+
+                if (_right_force_point.GlobalPosition.Y > 0) {
+                    force = Vector3.Zero;
+                }
+
+                ApplyForce(-0.125f * force * forward, _right_paddle.Position);
+                ApplyCentralForce(-4 * _right_paddle_angular_velocity.Sign().X * forward);
+                break;
+            }
         }
-
-        if (!_right_paddle_timer.IsStopped()) {
-            float delta_angle = Mathf.DegToRad(rpa) * (float)delta;
-            rotation = rotation + new Vector3(0, delta_angle, 0);
-            velocity += forward * rpf * power * (float)delta;
-            //velocity = velocity.Rotated(axis, rotation.Y * (float)delta);
-        }
-
-        _pivot.Rotation = rotation;
-        Velocity = velocity - velocity * 0.95f * (float)delta;
-        DebugDraw3D.DrawLine(Position + Vector3.Up, Position + Velocity + Vector3.Up, Colors.Red);
-        DebugDraw3D.DrawLine(Position + Vector3.Up, Position + Vector3.Forward.Rotated(axis, rotation.Y) + Vector3.Up, Colors.Blue);
-        MoveAndSlide();
-    }
-
-    public void _on_left_paddle_timer_timeout() {
-
-    }
-
-    public void _on_right_paddle_timer_timeout() {
-
     }
 }

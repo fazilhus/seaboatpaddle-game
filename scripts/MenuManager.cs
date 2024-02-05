ad1671652e0a9ecc140339cb294d0f218a720b5a
Dennis Karlsson <89897864+SlasherSRL@users.noreply.github.com>
2024-02-05 13:21:42 +0100

    Additional changes
    Added so you can load into another scene on button press in main menu

--- scripts/MenuManager.cs
+++ scripts/MenuManager.cs
@@ -5,9 +5,14 @@ public partial class MenuManager : MarginContainer
 {
     public override void _Ready()
     {
-        GetNode<Button>("HorizontalContainer/MenuContainer/Select").GrabFocus();
+        GetNode<Button>("HorizontalContainer/MenuContainer/Select").GrabFocus(); 
     }
-    
+    public void onSelectButtonPressed()
+    {
+        GetTree().ChangeSceneToFile("res://scenes/main.tscn"); //Current implementation only.
+        //Should be changed to hopefully load scene and disable Current menu to make it easier for managers to keep data
+        // We probably wouldnt want to change 
+    }
     public void onExitButtonPressed()
     {
 

9dbef9a76513de15bf4fb2f5b07f0fbac8309ad9
unkbitz <149374477+unkbitz@users.noreply.github.com>
2024-02-13 10:53:52 +0100

    making modifier crates

--- scripts/Boat.cs
+++ scripts/Boat.cs
@@ -4,24 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 
 enum PaddleSide {
-    Left = 0,
-    Right = 1,
+	Left = 0,
+	Right = 1,
 }
 
 public partial class Boat : RigidBody3D
 {
-    [Export]
-    public Godot.Collections.Array<Node3D> paddles;
+	[Export]
+	public Godot.Collections.Array<Node3D> paddles;
 
-    private List<Vector3> _player_inputs;
-    private List<Vector3> _paddles_rotation_old;
+	private List<Vector3> _player_inputs;
+	private List<Vector3> _paddles_rotation_old;
 
-    [Export]
-    public float sideways_force_ratio = 0.5f;
-    [Export]
-    public float forward_force_ratio = 50;
-    
-    //BoatPhysicsVariables
+	[Export]
+	public float sideways_force_ratio = 0.5f;
+	[Export]
+	public float forward_force_ratio = 50;
+	
+	//BoatPhysicsVariables
 
 	[Export] private float floatForce = 1.0f;
 	[Export] private float waterDrag = 0.005f;
@@ -42,107 +42,107 @@ public partial class Boat : RigidBody3D
 	public double time = 0;
 
 	public Godot.Collections.Array<Node> probeContainer;
-    
-    [Export] Survivors survivors;
+	
+	[Export] Survivors survivors;
 
-    public override void _Ready()
-    {
-        //instantiate variables for boat physics
-        var parent = GetParent();
-        gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
+	public override void _Ready()
+	{
+		//instantiate variables for boat physics
+		var parent = GetParent();
+		gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
 		//water = parent.GetNode<WaterPlane>("WaterPlane");
 		probeContainer = GetNode<Node3D>("ProbeContainer").GetChildren();
-        survivors = parent.GetNode<Survivors>("Survivors");
-        
+		survivors = parent.GetNode<Survivors>("Survivors");
+		
 	
 		initialY = GlobalPosition.Y;
 
-        _player_inputs = new List<Vector3>();
-        _paddles_rotation_old = new List<Vector3>();
-        foreach (int device_id in Input.GetConnectedJoypads()) {
-            _player_inputs.Add(Vector3.Zero);
-            _paddles_rotation_old.Add(Vector3.Zero);
-        }
-    }
+		_player_inputs = new List<Vector3>();
+		_paddles_rotation_old = new List<Vector3>();
+		foreach (int device_id in Input.GetConnectedJoypads()) {
+			_player_inputs.Add(Vector3.Zero);
+			_paddles_rotation_old.Add(Vector3.Zero);
+		}
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector3 forward = Basis.Z;
-        foreach (var it in paddles.Select((paddle, i) => new {Paddle = paddle, Index = i})) {
-            if (it.Index >= _player_inputs.Count) {
-                continue;
-            }
-            
-            Vector3 input = GetPlayerInput(it.Index);
-            //Vector3 input = _player_inputs[it.Index];
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 forward = Basis.Z;
+		foreach (var it in paddles.Select((paddle, i) => new {Paddle = paddle, Index = i})) {
+			if (it.Index >= _player_inputs.Count) {
+				continue;
+			}
+			
+			Vector3 input = GetPlayerInput(it.Index);
+			//Vector3 input = _player_inputs[it.Index];
 
-            _paddles_rotation_old[it.Index] = it.Paddle.Rotation;
-            it.Paddle.Rotation = new Vector3(input.X * 0.8f, 0, input.Z * 0.5f);
-            Vector3 angular_velocity = (it.Paddle.Rotation - _paddles_rotation_old[it.Index]) / (float)delta;
-            Vector3 force = new Vector3(angular_velocity.Z, 0, -angular_velocity.X);
+			_paddles_rotation_old[it.Index] = it.Paddle.Rotation;
+			it.Paddle.Rotation = new Vector3(input.X * 0.8f, 0, input.Z * 0.5f);
+			Vector3 angular_velocity = (it.Paddle.Rotation - _paddles_rotation_old[it.Index]) / (float)delta;
+			Vector3 force = new Vector3(angular_velocity.Z, 0, -angular_velocity.X);
 
-            Node3D force_point = it.Paddle.GetNode<Node3D>("ForcePoint");
-            if (force_point.GlobalPosition.Y < GlobalPosition.Y) {
-                ApplyForce(-sideways_force_ratio * force, it.Paddle.Position);
-                ApplyCentralForce(-forward_force_ratio * Curve(force) * force.Sign().Z * forward);
-            }
-        }              
+			Node3D force_point = it.Paddle.GetNode<Node3D>("ForcePoint");
+			if (force_point.GlobalPosition.Y < GlobalPosition.Y) {
+				ApplyForce(-sideways_force_ratio * force, it.Paddle.Position);
+				ApplyCentralForce(-forward_force_ratio * Curve(force) * force.Sign().Z * forward);
+			}
+		}              
 
-        // checking marker3d in the probe container for simulating the water physics
-        isSubmerged = false;
+		// checking marker3d in the probe container for simulating the water physics
+		isSubmerged = false;
 		foreach(Marker3D p in probeContainer)
 		{
 			float depth = (float)water.getHeight(GlobalPosition) - p.GlobalPosition.Y;
 
-            if(depth > 0)
-            {
-            isSubmerged = true;
-            ApplyForce(Vector3.Up * floatForce * gravity * depth, p.GlobalPosition - GlobalPosition);
-            }
+			if(depth > 0)
+			{
+			isSubmerged = true;
+			ApplyForce(Vector3.Up * floatForce * gravity * depth, p.GlobalPosition - GlobalPosition);
+			}
 		}
 
 
-        
-    }
-    public override void _IntegrateForces(PhysicsDirectBodyState3D state) // changing the simulation state of the object
-    {
+		
+	}
+	public override void _IntegrateForces(PhysicsDirectBodyState3D state) // changing the simulation state of the object
+	{
 		if(isSubmerged)
 		{
 			state.LinearVelocity *= 1 - waterDrag;
 			state.AngularVelocity *= 1 - WaterAngularDrag;
 		}
 
-        if (state.LinearVelocity.Length() >= 8) {
-            state.LinearVelocity = 8 * state.LinearVelocity.Normalized();
-        }
-    }
+		if (state.LinearVelocity.Length() >= 8) {
+			state.LinearVelocity = 8 * state.LinearVelocity.Normalized();
+		}
+	}
 
-    private float Curve(Vector3 v) {
-        return Mathf.Pow(v.Length() / 10, 2);
-    }
+	private float Curve(Vector3 v) {
+		return Mathf.Pow(v.Length() / 10, 2);
+	}
 
-    private Vector3 GetPlayerInput(int device_id) {
-        Vector3 input = Vector3.Zero;
-        switch (device_id) {
-            case 0: {
-                input.Z = Input.GetAxis("right_p1", "left_p1");
-                input.X = -Input.GetAxis("backward_p1", "forward_p1");
-                break;
-            }
-            case 1: {
-                input.Z = Input.GetAxis("right_p2", "left_p2");
-                input.X = -Input.GetAxis("backward_p2", "forward_p2");
-                break;
-            }
-        }
-        return input;
-    }
-    public void OnArea3dTriggerBoatAreaEntered(Area3D area)
-    {
-        if(area.IsInGroup("Survivors"))
-        {
-              GD.Print("boat is colliding with survivors!");
-        }
-      
-    }
+	private Vector3 GetPlayerInput(int device_id) {
+		Vector3 input = Vector3.Zero;
+		switch (device_id) {
+			case 0: {
+				input.Z = Input.GetAxis("right_p1", "left_p1");
+				input.X = -Input.GetAxis("backward_p1", "forward_p1");
+				break;
+			}
+			case 1: {
+				input.Z = Input.GetAxis("right_p2", "left_p2");
+				input.X = -Input.GetAxis("backward_p2", "forward_p2");
+				break;
+			}
+		}
+		return input;
+	}
+	public void OnArea3dTriggerBoatAreaEntered(Area3D area)
+	{
+		if(area.IsInGroup("Survivors"))
+		{
+			  GD.Print("boat is colliding with survivors!");
+		}
+	  
+	}
 }
--- scripts/Modifiers.cs
+++ scripts/Modifiers.cs
@@ -1,9 +1,9 @@
 using Godot;
 using System;
-using System.Security.Cryptography.X509Certificates;
 
-public partial class Survivors : RigidBody3D
+public partial class Modifiers : RigidBody3D
 {
+	
 	[Export] private float floatForce = 1.0f;
 	[Export] private float waterDrag = 0.005f;
 	[Export] private float WaterAngularDrag = 0.01f;
@@ -14,25 +14,22 @@ public partial class Survivors : RigidBody3D
 	private double elapsedTime = 0;
 	[Export] public WaterPlane water;
 	[Export] public Area3D boatCollider;
-	[Export] public Area3D survivorsCollider;
+	[Export] public Area3D modifiersCollider;
 	public double time = 0;
-
+	
 	public Godot.Collections.Array<Node> probeContainer;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		var parent = GetParent();
 		gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
-		//water = GetNode<WaterPlane>("/root/Main/WaterPlane");
 		probeContainer = GetNode<Node3D>("ProbeContainer").GetChildren();
-		
-
-	
 		initialY = GlobalPosition.Y;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double delta)
+	public override void _Process(double delta)
 	{
 		isSubmerged = false;
 		foreach(Marker3D p in probeContainer)
@@ -43,10 +40,10 @@ public partial class Survivors : RigidBody3D
 			 {
 				isSubmerged = true;
 				ApplyForce(Godot.Vector3.Up * floatForce * gravity * depth, p.GlobalPosition - GlobalPosition);
-			 }
-				
+			 }	
 		}
 	}
+	
 	public override void _IntegrateForces(PhysicsDirectBodyState3D state) // changing the simulation state of the object
 	{
 		if(isSubmerged)
@@ -55,12 +52,14 @@ public partial class Survivors : RigidBody3D
 			state.AngularVelocity *= 1 - WaterAngularDrag;
 		}
 	}
-	public void OnArea3DTiggerAreaEntered(Area3D area)
+	
+	public void OnArea3dTriggerAreaEntered(Area3D area)
 	{
 		if (area.IsInGroup("ThePlayers"))
 		{
-			GD.Print("ThePlayers are colliding with survivors");
+			GD.Print("ThePlayers are colliding with modifiers");
 			QueueFree();
+			//Add functions calls for actuall modifiers here
 		}
 	
 	}
